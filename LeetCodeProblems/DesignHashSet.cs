9b347658e8808c7874de644cbbf4f7d88c0f9f9e
maqsudj0n1 <maqsudhaydarov1203@gmail.com>
2023-01-26 23:27:50 +0500

    New

---
+++ LeetCodeProblems/DesignHashSet.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+
+public class MyHashSet
+{
+    ArrayList list;
+
+    public MyHashSet() => list = new ArrayList();
+
+    public void Add(int key) { if (!Contains(key)) list.Add(key); }
+
+    public void Remove(int key) { if (Contains(key)) list.Remove(key); }
+
+    public bool Contains(int key) { return list.Contains(key); }
+}
+
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.Add(key);
+ * obj.Remove(key);
+ * bool param_3 = obj.Contains(key);
+ */
---
+++ LeetCodeProblems/IntersectionOfTwoLinkedList.cs
@@ -0,0 +1,21 @@
+﻿public class Solution
+{
+    public ListNode GetIntersectionNode(ListNode headA, ListNode headB)
+    {
+        List<ListNode> list1 = new List<ListNode>();
+        List<ListNode> list2 = new List<ListNode>();
+        ListNode temp1 = headA;
+        ListNode temp2 = headB;
+        while (temp1 is not null)
+        {
+            list1.Add(temp1);
+            temp1 = temp1.next;
+        }
+        while (temp2 is not null)
+        {
+            list2.Add(temp2);
+            temp2 = temp2.next;
+        }
+        return list1.FirstOrDefault(x => list2.Contains(x));
+    }
+}
